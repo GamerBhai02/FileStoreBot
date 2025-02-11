19a2bb8915322facb4433909e082679a90c870ab
Abu Talha Ansari <101631188+GamerBhai02@users.noreply.github.com>
2025-02-11 20:46:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,20 +1,10 @@
-#TitanXBots
-#Credit
-#yash
-
-
-
 import pymongo, os
 from config import DB_URI, DB_NAME
-
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 
-
 user_data = database['users']
-
-
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
@@ -32,7 +22,6 @@ async def full_userbase():
     user_ids = []
     for doc in user_docs:
         user_ids.append(doc['_id'])
-        
     return user_ids
 
 async def del_user(user_id: int):
